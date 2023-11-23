d4f3125a71773333f2d99c7de7891d04d42c33f6
abishekpathak-bitkraft <abhishek.pathak@bitkraft.co.in>
2023-11-23 12:04:10 +0530

    added

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,4 @@
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
-import { jwtDecode } from "jwt-decode";
 import { useNavigate } from "react-router-dom";
 
 function Login({ setUser }: any) {
@@ -13,13 +12,28 @@ function Login({ setUser }: any) {
         }}
       >
         <h1>Login</h1>
-        <GoogleOAuthProvider clientId="">
+        <GoogleOAuthProvider clientId="259636043206-uliarepqr5uf1pqqci8ektl2tuim58j3.apps.googleusercontent.com">
           <GoogleLogin
-            onSuccess={(credentialResponse) => {
-              const decoded = jwtDecode(credentialResponse.credential);
-              console.log("user: ", decoded);
-              setUser(decoded);
-              navigate("/dashboard")
+            onSuccess={async (credentialResponse) => {
+              console.log("cred ", credentialResponse);
+
+              const token = credentialResponse.credential;
+              console.log("token: ", token);
+              // console.log(token);
+              // navigate("/dashboard")
+              let res = await fetch(
+                "http://localhost:8080/api/v1/auth/google",
+                {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({ token }),
+                }
+              );
+              let result = await res.json();
+
+              console.log("result: ", result);
             }}
             onError={() => {
               console.log("Login Failed");
