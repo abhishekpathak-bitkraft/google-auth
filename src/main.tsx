d60f500904508bc92d638a6630bd1a8c68c49f71
abishekpathak-bitkraft <abhishek.pathak@bitkraft.co.in>
2023-11-24 13:10:31 +0530

    new method

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <GoogleOAuthProvider clientId="259636043206-uliarepqr5uf1pqqci8ektl2tuim58j3.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>
+);
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,30 @@
-import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import {
+  GoogleOAuthProvider,
+  GoogleLogin,
+  useGoogleLogin,
+} from "@react-oauth/google";
 import { useNavigate } from "react-router-dom";
 
 function Login({ setUser }: any) {
   const navigate = useNavigate();
+
+  const login = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      console.log(tokenResponse);
+
+      let res = await fetch("http://localhost:8080/api/v1/auth/google", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          token: tokenResponse.code,
+        }),
+      });
+      console.log("res: ", res);
+    },
+    flow: "auth-code",
+  });
 
   return (
     <>
@@ -12,34 +34,37 @@ function Login({ setUser }: any) {
         }}
       >
         <h1>Login</h1>
-        <GoogleOAuthProvider clientId="259636043206-uliarepqr5uf1pqqci8ektl2tuim58j3.apps.googleusercontent.com">
-          <GoogleLogin
-            onSuccess={async (credentialResponse) => {
-              console.log("cred ", credentialResponse);
+        <h1>apple</h1>
+        <div id="apple">
+          {/* <GoogleLogin
+              text="continue_with"
+              onSuccess={async (credentialResponse) => {
+                console.log("cred ", credentialResponse);
 
-              const token = credentialResponse.credential;
-              console.log("token: ", token);
-              // console.log(token);
-              // navigate("/dashboard")
-              let res = await fetch(
-                "http://localhost:8080/api/v1/auth/google",
-                {
-                  method: "POST",
-                  headers: {
-                    "Content-Type": "application/json",
-                  },
-                  body: JSON.stringify({ token }),
-                }
-              );
-              let result = await res.json();
+                const token = credentialResponse.credential;
+                console.log("token: ", token);
+                // console.log(token);
+                // navigate("/dashboard")
+                let res = await fetch(
+                  "http://localhost:8080/api/v1/auth/google",
+                  {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ token }),
+                  }
+                );
+                let result = await res.json();
 
-              console.log("result: ", result);
-            }}
-            onError={() => {
-              console.log("Login Failed");
-            }}
-          />
-        </GoogleOAuthProvider>
+                console.log("result: ", result);
+              }}
+              onError={() => {
+                console.log("Login Failed");
+              }}
+            /> */}
+          <button onClick={() => login()}>Sign in with Google 🚀 </button>;
+        </div>
       </div>
     </>
   );
